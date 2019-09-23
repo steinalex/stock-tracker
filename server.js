c1b4b2bb55739bf6b148a2f43becf43f97b48b18
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-09-23 10:19:01 +0100

    list all objects from one source

--- server.js
+++ server.js
@@ -36,7 +36,24 @@ const getApiAndEmit = async (socket, stockName) => {
         `https://cloud.iexapis.com/stable/stock/${stockName}/quote?token=sk_72576fe17dd04de4907aff14eb6507c2`
       );
       const test = {
-        companyName: res.data.companyName
+        companyName: res.data.companyName,
+        symbol:res.data.symbol,
+        primaryExchange:res.data.primaryExchange,
+        latestPrice:res.data.latestPrice,
+        latestTime:res.data.latestTime,
+        previousClose:res.data.previousClose,
+        previousVolume:res.data.previousVolume,
+        change:res.data.change,
+        changePercent:res.data.changePercent,
+        avgTotalVolume:res.data.avgTotalVolume,
+        marketCap:res.data.marketCap,
+        peRatio:res.data.peRatio,
+        week52High:res.data.week52High,
+        week52Low:res.data.week52Low,
+        ytdChange:res.data.ytdChange,
+        isUSMarketOpen:res.data.isUSMarketOpen
+
+
       }
 
       socket.emit("FromAPI", test);
