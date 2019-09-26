6350f94fff182abc9c7ae26494fee97d461112bf
Alex Stein <alex.stein@weareadaptive.com>
2019-09-26 10:46:13 +0100

    Added table styling for key stats

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -6,10 +6,10 @@ const Headline = () => {
     const response = useSelector((state) => state.response)
 
     return (
-        <>
+        <div className="header">
             <h1>{response.companyName} ({response.symbol}) {response.latestPrice} {response.change} {response.changePercent} </h1>
             <h2>{response.primaryExchange} {response.sector} {response.currency}</h2>
-        </>
+        </div>
     )
 }
 
--- src/components/KeyStats.js
+++ src/components/KeyStats.js
@@ -3,61 +3,48 @@ import { useSelector } from 'react-redux';
 
 const KeyStats = () => {
 
-  const response = useSelector((state) => state.response)
-  return (
-    <div className="key-stats">
-      <h1>Key Stats</h1>
-      <table  >
-        <tbody>
-          <tr>
-            <td>Previous Close:</td><td>{response.previousClose}</td>
-          </tr>
-          <tr>
-            <td>Day Range:</td> <td>{response.low}-{response.high}</td>
-          </tr>
-          <tr>
-            <td>Volume:</td> <td>{response.previousVolume}</td>
-          </tr>
-          <tr>
-            <td>Market Cap:</td> <td> {response.marketCap} </td>
-          </tr>
-          <tr>
-            <td>P/E Ratio:</td> <td> {response.peRatio} </td>
-          </tr>
-          <tr>
-            <td>Open:</td> {response.open}
-          </tr>
-          <tr>
-            <td>52 Week Range:</td> <td>{response.week52High}-{response.week52Low}</td>
-          </tr>
-          <tr>
-            <td>Total Avg Volume:</td> <td>{response.avgTotalVolume}</td>
-          </tr>
-          <tr>
-            <td>Earning per share:</td> <td> {response.EPS}</td>
-          </tr>
-          <tr>
-            <td>Dividend & Yield:</td> <td> {response.ytdChange}</td>
-          </tr>
-          <tr>
-            <td> Open: </td> <td>{response.open} </td>
-          </tr>
-          <tr>
-            <td>52 Week Range:</td> <td> {response.week52High}-{response.week52Low}</td>
-          </tr>
-          <tr>
-            <td>Total Avg Volume:</td> <td>{response.avgTotalVolume}</td>
-          </tr>
-          <tr>
-            <td>Earning per share:</td> <td> {response.EPS}</td>
-          </tr>
-          <tr>
-            <td>Dividend & Yield:</td> <td>{response.ytdChange}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
+    const response = useSelector((state) => state.response)
+    return (
+        <div className="key-stats">
+            <h1>Key Stats</h1>
+            <div className="key-stats__wrapper">
+                <table className="key-stats__table">
+                    <tbody>
+                        <tr>
+                            <td>Previous Close:</td><td>{response.previousClose}</td>
+                        </tr>
+                        <tr>
+                            <td>Day Range:</td> <td>{response.low}-{response.high}</td>
+                        </tr>
+                        <tr>
+                            <td>Volume:</td> <td>{response.previousVolume}</td>
+                        </tr>
+                        <tr>
+                            <td>Market Cap:</td> <td> {response.marketCap} </td>
+                        </tr>
+                        <tr>
+                            <td>P/E Ratio:</td> <td> {response.peRatio} </td>
+                        </tr>
+                        <tr>
+                            <td>Open:</td> <td>{response.open}</td>
+                        </tr>
+                        <tr>
+                            <td>52 Week Range:</td> <td>{response.week52High}-{response.week52Low}</td>
+                        </tr>
+                        <tr>
+                            <td>Total Avg Volume:</td> <td>{response.avgTotalVolume}</td>
+                        </tr>
+                        <tr>
+                            <td>Earning per share:</td> <td> {response.EPS}</td>
+                        </tr>
+                        <tr>
+                            <td> Open: </td> <td>{response.open} </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default KeyStats
