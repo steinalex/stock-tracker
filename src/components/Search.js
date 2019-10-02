462cfca797864f37c3195317db04d6c70089469f
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-10-02 10:16:23 +0100

    search bar update

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,29 +6,41 @@ const Search = ({updateStock}) => {
 
     const dispatch = useDispatch()
     const state = useSelector((state) => state)
-
     const filteredSymbols = state.selectedCompanySymbols
-    console.log(filteredSymbols)
-    // const name = filteredSymbols.map(data => data.name)
-    // const symbol = filteredSymbols.map(data => data.symbol)
-    // const nameSymbol = filteredSymbols.map(data => (data.name + ' ('+ data.symbol + ')'))
-
     const [stock, setStock] = useState('');
-    
+    const [clickState, setClickState] = useState(false);
+    const [searchBarState, setsearchBarState] = useState('');
+    // const displayDropdown = (stock) => {display: stock.length > 0 ?'block':'none'}
     const onChange = event => {
         setStock(event.target.value)
         dispatch(updateSearchQueryAction(event.target.value))
     }
 
     const onSubmit = ({key, target}) => {
+      console.log(target)
         if (key === 'Enter') {
-            updateStock(target.value)
+          updateStock(target.value)
         }
+      }
+      
+      const optionClick = data => {
+        setStock(` ${data.name} (${data.symbol})`)
+        updateStock(data.symbol)
+        // setClickState(true)
     }
 
-    const optionClick = data => {
-      console.log('DATA', data.symbol)
-      setStock(data.symbol)
+    const updateSeachBar = () => {
+      console.log("Click state", clickState)
+      console.log("Length", stock.length)
+      if (!stock.length) {
+        return {display: 'none'}
+      }
+      else if (stock.length > 0) {
+        return {display: 'block'}
+      }
+      else if (stock.length > 0 && clickState === true) {
+        return {display: 'none'}
+      }
     }
 
     const options = filteredSymbols.map(data => {
@@ -44,7 +56,7 @@ const Search = ({updateStock}) => {
         <div className="search-bar">
             <input type="text" placeholder="Search..." className="search-bar__input" value={stock} onChange={onChange} onKeyPress={onSubmit}/>
         </div>
-        <ul name="search" className="search-bar__options">{options}</ul>
+        <ul name="search" className="search-bar__options" style={updateSeachBar()} >{options}</ul>
         </>
     )
 }
