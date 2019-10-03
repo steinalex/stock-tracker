b8e60ea0862e489e2dce951515b375b7ac5d44cb
Alex Stein <alex.stein@weareadaptive.com>
2019-10-03 17:32:19 +0100

    Search icon tweaks and font size changes

--- src/components/MarketStatus.js
+++ src/components/MarketStatus.js
@@ -4,10 +4,10 @@ const marketSign = (value) => value === null ? 'moon' : 'sun'
 export const MarketStatus = ({stock, keyStats}) => {
     const UKTime = new Date(stock.latestUpdate)
     const USTime = new Date(UKTime).toLocaleString("en-US", {timeZone: "America/New_York"})
-    const marketStatus= keyStats.open === null ? 'Market Close' : 'Market Open'
+    const marketStatus= keyStats.open === null ? 'Market Closed' : 'Market Open'
   
     return (
     <div className="market">
-       <p>Real-Time Price as of {USTime} EST <span className={marketSign(keyStats.open)}> {marketStatus}</span></p>
+       <p><span className="market-text">Real-Time Price as of {USTime} EST</span> <span className={marketSign(keyStats.open)}> {marketStatus}</span></p>
 </div>
 )}
