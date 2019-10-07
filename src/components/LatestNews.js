474a85ae07e32531c5ae4c062cad8f3cf67a0fae
Alex Stein <alex.stein@weareadaptive.com>
2019-10-07 14:35:14 +0100

    Added link safety and changed keystats back to old table

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -12,7 +12,7 @@ const LatestNews = ({ stock }) => {
                 <tbody>
                     {stock.map(data =>
                         <>
-                            <tr><td><a target="_blank" className='news' href={`${data.url}`}>{data.headline}</a></td></tr>
+                            <tr><td><a target="_blank" rel="noopener noreferrer" className='news' href={`${data.url}`}>{data.headline}</a></td></tr>
                             <tr><td>{timeFormat(data.date)} - {data.source} </td></tr>
                         </>
                     )}
