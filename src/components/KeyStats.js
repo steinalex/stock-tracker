ab80d8d445603060a5a36ffc0568bc360cdfb876
Alex Stein <alex.stein@weareadaptive.com>
2019-10-07 11:47:30 +0100

    Made a start on dynamic rendering for tables

--- src/components/KeyStats.js
+++ src/components/KeyStats.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 const NUMBER_FORMATTER = new Intl.NumberFormat();
 
-const formatNumber = (num) => { return NUMBER_FORMATTER.format(num)}
+const formatNumber = (num) => { return NUMBER_FORMATTER.format(num) }
 
 const KeyStats = ({ stock }) => {
     const stockOpen = stock.open == null ? 'N/A' : stock.open
     const dayRange = stock.low == null ? 'N/A' : `${stock.low} - ${stock.high}`
     const dividend = (stock.ytdChange * 100).toPrecision(3)
+
+    const generateKeyStatsTable = () => {
+        return Object.keys(stock).map((obj, index) => {
+            return (
+                <tr key={index}>
+                    <td>{obj}</td>
+                    <td>{stock[obj]}</td>
+                </tr>
+            )
+        })
+    }
 
     return (
         <div className="key-stats">
@@ -15,36 +26,7 @@ const KeyStats = ({ stock }) => {
             <div className="key-stats__wrapper">
                 <table className="key-stats__table">
                     <tbody>
-                        <tr>
-                            <td>Previous Close:</td><td>{stock.previousClose}</td>
-                        </tr>
-                        <tr>
-                            <td>Day Range:</td><td>{dayRange}</td>
-                        </tr>
-                        <tr>
-                            <td>Volume:</td><td>{formatNumber(stock.previousVolume)}</td>
-                        </tr>
-                        <tr>
-                            <td>Market Cap:</td><td>{formatNumber(stock.marketCap)}</td>
-                        </tr>
-                        <tr>
-                            <td>P/E Ratio:</td><td>{stock.peRatio}</td>
-                        </tr>
-                        <tr>
-                            <td>Open:</td><td>{stockOpen}</td>
-                        </tr>
-                        <tr>
-                            <td>52 Week Range:</td><td>{stock.week52High} - {stock.week52Low}</td>
-                        </tr>
-                        <tr>
-                            <td>Total Avg Volume:</td><td>{formatNumber(stock.avgTotalVolume)}</td>
-                        </tr>
-                        <tr>
-                            <td>Earning per share:</td><td>{stock.eps}</td>
-                        </tr>
-                        <tr>
-                            <td> Dividend &amp; Yield:</td><td>{dividend} %</td>
-                        </tr>
+                        {generateKeyStatsTable()}
                     </tbody>
                 </table>
             </div>
@@ -53,3 +35,36 @@ const KeyStats = ({ stock }) => {
 }
 
 export default KeyStats
+
+                    // <tbody>
+                    //     <tr>
+                    //         <td>Previous Close:</td><td>{stock.previousClose}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>Day Range:</td><td>{dayRange}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>Volume:</td><td>{formatNumber(stock.previousVolume)}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>Market Cap:</td><td>{formatNumber(stock.marketCap)}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>P/E Ratio:</td><td>{stock.peRatio}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>Open:</td><td>{stockOpen}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>52 Week Range:</td><td>{stock.week52High} - {stock.week52Low}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>Total Avg Volume:</td><td>{formatNumber(stock.avgTotalVolume)}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td>Earning per share:</td><td>{stock.eps}</td>
+                    //     </tr>
+                    //     <tr>
+                    //         <td> Dividend &amp; Yield:</td><td>{dividend} %</td>
+                    //     </tr>
+                    // </tbody>
