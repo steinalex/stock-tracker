1e321ec1f4fecdede20fb0065436ff5012772957
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-10-07 11:38:24 +0100

    add url to news sections

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import moment from 'moment';
 
+
+const timeFormat = (date) => moment(date).fromNow()
 const LatestNews = ({stock}) => {
     console.log(stock)
     // const news = stock.map((data, index) =>({headline: data.headline, url : data.url}))
     // console.log(news)
-    const a= 1603355187453;
-    const b=moment(a).fromNow();
-    console.log(b)
-    const timeFormat = (date) => moment(date).fromNow()
     // {moment(news.datetime).fromNow()}
 
     return (
@@ -16,15 +14,15 @@ const LatestNews = ({stock}) => {
             <h1 className="title">Latest News</h1>
             <table>
                 <tbody>
-                    <tr><td>{stock.news1}</td></tr>
+                    <tr><td><a href={`${stock.news1url}`}>{stock.news1}</a></td></tr>
                     <tr><td>{timeFormat(stock.news1Date)} {stock.news1Source} </td></tr>
-                    <tr><td>{stock.news2}</td></tr>
+                    <tr><td><a href={`${stock.news2url}`}>{stock.news2}</a></td></tr>
                     <tr><td>{timeFormat(stock.news2Date)} {stock.news2Source} </td></tr>
-                    <tr><td>{stock.news3}</td></tr>
+                    <tr><td><a href={`${stock.news3url}`}>{stock.news3}</a></td></tr>
                     <tr><td>{timeFormat(stock.news3Date)} {stock.news3Source}</td></tr>
-                    <tr><td>{stock.news4}</td></tr>
+                    <tr><td><a href={`${stock.news4url}`}>{stock.news4}</a></td></tr>
                     <tr><td>{timeFormat(stock.news4Date)} {stock.news4Source} </td></tr>
-                    <tr><td>{stock.news5}</td></tr>
+                    <tr><td><a href={`${stock.news5url}`}>{stock.news5}</a></td></tr>
                     <tr><td>{timeFormat(stock.news5Date)} {stock.news5Source}</td></tr>
                 </tbody>
             </table>
