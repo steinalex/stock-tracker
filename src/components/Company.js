90bb665cce34025b095e1c88233155c44741defc
Alex Stein <alex.stein@weareadaptive.com>
2019-10-09 14:00:17 +0100

    Component titles show on load

--- src/components/Company.js
+++ src/components/Company.js
@@ -5,12 +5,17 @@ import { useSelector } from 'react-redux';
 
 export const Company = ({ stock }) => {
     const state = useSelector((state) => state)
-    return stock.length === 0 ? <div className="loading-spinner"></div> :
+    return (
         <div className="company-overview">
             <h1 className="title">Company Overview</h1>
-            <div className="company-overview__title">{stock.companyName} ({stock.symbol})</div>
-            <div><a target='_blank' rel="noopener noreferrer" className='company-overview__website' href={`${stock.website}`}>{stock.website}</a> </div>
-            <div className="company-overview__text"><p>{stock.description}</p></div>
+            {stock.length === 0 ? <div className="loading-spinner"></div> :
+                <>
+                    <div className="company-overview__title">{stock.companyName} ({stock.symbol})</div>
+                    <div><a target='_blank' rel="noopener noreferrer" className='company-overview__website' href={`${stock.website}`}>{stock.website}</a> </div>
+                    <div className="company-overview__text"><p>{stock.description}</p></div>
+                </>
+            }
             <Peers stock={state.selectedTopPeers} />
-        </div >
+        </div>
+    )
 }
--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -2,21 +2,23 @@ import React from 'react';
 import moment from 'moment';
 
 
-const timeFormat = (date) => moment(date).fromNow()
-const LatestNews = ({ stock }) => {
-    return stock.length === 0 ? <div className="loading-spinner"></div> :
+const timeFormat = (date) => moment(date).fromNow();
 
+const LatestNews = ({ stock }) => (
         <div className="latest-news">
             <h1 className="title">Latest News</h1>
-            <div className="latest-news__grid">
-                {stock.map(data =>
-                    <div className="latest-news__wrapper">
-                        <div className="latest-news__text"><a target="_blank" rel="noopener noreferrer" className='latest-news__link' href={`${data.url}`}>{data.headline}</a></div>
-                        <div className="latest-news__source">{timeFormat(data.date)} - {data.source}</div>
-                    </div>
-                )}
-            </div>
-        </div>
-}
+            {stock.length === 0
+                ? <div className="loading-spinner"></div>
+                : <div className="latest-news__grid">
+                    {stock.map(data =>
+                        <div className="latest-news__wrapper">
+                            <div className="latest-news__text"><a target="_blank" rel="noopener noreferrer" className='latest-news__link' href={`${data.url}`}>{data.headline}</a></div>
+                            <div className="latest-news__source">{timeFormat(data.date)} - {data.source}</div>
+                        </div>
+                    )}
+                </div>
+            }
+    </div> 
+)
 
 export default LatestNews
