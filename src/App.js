49797f07b018d09a025b1c1ed1b0e55ce2a61fea
Alex Stein <alex.stein@weareadaptive.com>
2019-10-09 12:15:37 +0100

    Basic loader spinner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { updateChartAction } from './store/actions';
-import {Headline} from "./components/Headline";
+import { Headline } from "./components/Headline";
 import Chart from "./components/Chart";
 import LatestNews from "./components/LatestNews"
 import KeyStats from "./components/KeyStats"
-import {Company} from "./components/Company"
+import { Company } from "./components/Company"
 import { Footer } from "./components/Footer";
 
 const App = () => {
@@ -14,16 +14,24 @@ const App = () => {
 
   return (
     <>
-    <div className="grid-container">
-      <Headline stock={state.selectedSearch} />
-      <Chart stock={state.selectedChartData} latestPrice={state.selectedStockTicker} updateChartRange={(stock) => dispatch(updateChartAction(stock))}/>
-      <LatestNews stock={state.selectedLatestNews} />
-      <KeyStats stock={state.selectedKeyStats} />
-      <Company stock={state.selectedCompanyOverview} />
-    </div>
-    <div>
-      <Footer />
-    </div>
+      <div className="grid-container">
+        <Headline stock={state.selectedSearch} />
+        {state.selectedStock
+          ? <>
+            <Chart stock={state.selectedChartData} latestPrice={state.selectedStockTicker} updateChartRange={(stock) => dispatch(updateChartAction(stock))} />
+            <LatestNews stock={state.selectedLatestNews} />
+            <KeyStats stock={state.selectedKeyStats} />
+            <Company stock={state.selectedCompanyOverview} />
+          </>
+          : ''}
+      </div>
+      {state.selectedStock
+        ? <>
+          <div>
+            <Footer />
+          </div>
+        </>
+        : ''}
     </>
   );
 }
