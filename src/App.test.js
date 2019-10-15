61cc1240cc1ea3cd17f52ec280e02c590f9cc1d1
Alex Stein <alex.stein@weareadaptive.com>
2019-10-15 12:23:52 +0100

    Basic test created to test initial state of reducer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { reducer } from "./store/reducer";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// import { store } from "./store/store";
+// import { Provider } from "react-redux";
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe("Stock reducer", () => {
+  let previousState;
+  let newState;
+
+  describe("upon initialization", () => {
+    beforeEach(() => {
+      previousState = {
+        selectedStock: "",
+        selectedChartRange: "5y",
+        selectedKeyStats: "",
+        selectedLatestNews: null,
+        selectedCompanyOverview: null,
+        selectedTopPeers: null,
+        selectedSearch: "",
+        selectedCompanySymbols: [],
+        selectedQuotes: "",
+        selectedStockTicker: "",
+        selectedChartData: null,
+        enteredSearchQuery: ""
+      };
+    });
+
+    describe('and a stock "AAPL" name is entered', () => {
+      beforeEach(() => {
+        newState = reducer(previousState, "UPDATE_CHART_RANGE");
+      });
+      it("it sets selectedStock to the entered name", () => {
+        expect(newState.selectedStock).toBe("");
+      });
+    });
+  });
 });
