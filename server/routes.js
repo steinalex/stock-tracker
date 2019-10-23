6826a13e05113aebbe02daf625d4136de7cd3fa4
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-10-23 14:37:34 +0100

    delete routes unused part

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
+router.get("/", res => {
   res.send({ response: "Server up" }).status(200);
 });
 module.exports = router;
