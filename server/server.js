45fcda7895d1244ba524acfd6beffd4ec6a0001f
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-10-28 11:53:13 +0000

    move env file outsider server folder (#49)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
