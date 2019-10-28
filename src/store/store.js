7d8b6d183f6015af4e34f791485f918a977e63da
steinalex <54838842+steinalex@users.noreply.github.com>
2019-10-28 14:40:21 +0000

    Prevent render blocking by moving initial action to a callback (#50)

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { startupMiddleware, stockMiddleware } from "./middleware";
 import { combinedReducer } from "./reducer";
-import { bootstrap } from "./actions";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -10,5 +9,3 @@ export const store = createStore(
   undefined,
   composeEnhancers(applyMiddleware(...[stockMiddleware, startupMiddleware]))
 );
-
-store.dispatch(bootstrap());
