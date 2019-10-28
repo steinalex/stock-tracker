8aacc0f9d7e30a72fcac1c87af307f41fceaeebf
Alex Stein <alex.stein@weareadaptive.com>
2019-10-28 14:43:53 +0000

    Removed unused null

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -22,7 +22,7 @@ const HOST = window.location.hostname;
 const PORT = 4000;
 const SERVER = `${HOST}:${PORT}`;
 
-let socket = null;
+let socket;
 
 export const startupMiddleware = store => next => action => {
   if (action.type === BOOTSTRAP) {
