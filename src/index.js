884258265db9ae5ccee51798c483552554d541dd
Alex Stein <alex.stein@weareadaptive.com>
2019-10-28 14:23:14 +0000

    Prevent render blocking by moving initial action to a callback

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./styles/Variables.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { bootstrap } from "./store/actions";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 
@@ -12,7 +13,8 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById("root"),
+  () => store.dispatch(bootstrap())
 );
 
 // If you want your app to work offline and load faster, you can change
