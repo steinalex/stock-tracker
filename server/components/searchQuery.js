ec24424320dd5bf5364e40356499f9a89f136db5
Alex Stein <alex.stein@weareadaptive.com>
2019-10-30 15:26:03 +0000

    Search relevance added whilst searching for a symbol

--- server/components/searchQuery.js
+++ server/components/searchQuery.js
@@ -1,14 +1,22 @@
 exports.emitSearchQuery = async (socket, inputQuery, allSymbols) => {
   try {
     const symbols = await allSymbols;
-    const filteredData = symbols.filter(
-      search =>
-        search.symbol.toLowerCase().indexOf(inputQuery.toLowerCase()) !== -1 ||
-        search.name.toLowerCase().indexOf(inputQuery.toLowerCase()) !== -1
-    );
-    const topTen = filteredData.slice(0, 10);
 
-    socket.emit("companySymbols", topTen);
+    const filterData = symbols.filter(item => {
+      return item.symbol.toLowerCase().indexOf(inputQuery.toLowerCase()) !== -1;
+    });
+
+    const dataSort = filterData.sort((a, b) => {
+      const aStart =
+        a.symbol.match(new RegExp("^" + inputQuery.toUpperCase())) || [];
+      const bStart =
+        b.symbol.match(new RegExp("^" + inputQuery.toUpperCase())) || [];
+
+      if (aStart.length != bStart.length) return bStart.length - aStart.length;
+      else return a.symbol > b.symbol ? 1 : -1;
+    });
+
+    socket.emit("companySymbols", dataSort.slice(0, 10));
   } catch (error) {
     console.error(`Error: ${error}`);
   }
