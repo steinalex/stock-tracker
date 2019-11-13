637a7f97dbc6f94266d89b50eca5e03b2ceaceb1
Alex Stein <54838842+steinalex@users.noreply.github.com>
2019-11-13 11:35:44 +0000

    Fix for market status (#94)
    * Fix for market status
    
    * Removal of empty strings
    
    * Removal of console.log

--- src/features/market-status/components/MarketStatus.tsx
+++ src/features/market-status/components/MarketStatus.tsx
@@ -4,8 +4,8 @@ import "./MarketStatus.css";
 import { IKeyStats } from "../../key-stats/redux/actions";
 import { IStockTicker } from "../../stock-ticker/redux/actions";
 
-const marketSign = (value: number) =>
-  value === null ? "market---moon" : "market---sun";
+const marketSign = (value: boolean) =>
+  value === false ? "market---moon" : "market---sun";
 
 type MarketStatusProps = {
   stock: IStockTicker | undefined;
@@ -15,16 +15,20 @@ type MarketStatusProps = {
 export const MarketStatus: FC<MarketStatusProps> = ({ stock, keyStats }) => {
   const dateFormat = stock && moment(stock.latestUpdate).format("lll");
   const marketStatus =
-    keyStats && keyStats.open === null ? "Market Closed" : "Market Open";
+    keyStats && keyStats.isUSMarketOpen === false
+      ? "Market Closed"
+      : "Market Open";
 
   return (
     <div className="market">
       <p>
         <span className="market__text">
           Real-Time Price as of {dateFormat} EST
-        </span>{" "}
+        </span>
         {keyStats && (
-          <span className={marketSign(keyStats.open)}> {marketStatus}</span>
+          <span className={marketSign(keyStats.isUSMarketOpen)}>
+            {marketStatus}
+          </span>
         )}
       </p>
     </div>
