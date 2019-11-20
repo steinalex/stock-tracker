13b05a22cf5e67b7279f5f670646fa5d073c1b0f
Alex Stein <alex.stein@weareadaptive.com>
2019-11-20 10:39:00 +0000

    Middleware chart data now tests dispatch, on and emit

--- src/features/chart/redux/__test__/middleware.test.ts
+++ src/features/chart/redux/__test__/middleware.test.ts
@@ -1,5 +1,10 @@
 import { chartMiddleware } from "../middleware";
-import { updateChartRangeAction, updateChartDataAction } from "../actions";
+import {
+  updateChartRangeAction,
+  updateChartDataAction,
+  ChartData
+} from "../actions";
+import { BOOTSTRAP } from "store";
 
 jest.mock("services/socketService");
 
@@ -10,9 +15,12 @@ describe("When testing chart middleware", () => {
   let next: jest.Mock;
 
   beforeEach(() => {
+    const payload = [{ close: 1, date: "1" }];
     mockSocket = {
       emit: jest.fn(),
-      on: jest.fn()
+      on: jest.fn((name: string, callback: (payload: ChartData[]) => void) => {
+        callback(payload);
+      })
     };
 
     socketService = {
@@ -22,20 +30,32 @@ describe("When testing chart middleware", () => {
     const state = { stockData: { selectedStock: { symbol: "AAPL" } } };
 
     store = {
-      getState: jest.fn(() => state)
+      getState: jest.fn(() => state),
+      dispatch: jest.fn()
     };
 
     next = jest.fn();
   });
 
-  // it('it should create a socket on for the chart data', () => {
-  //     const action = updateChartDataAction([]);
-  //     const middleware = chartMiddleware({ socketService });
+  it("it should create a websocket for the chart data on startup", () => {
+    const action = { type: BOOTSTRAP };
+    const middleware = chartMiddleware({ socketService });
+    middleware(store)(next)(action);
 
-  //     middleware(store)(next)(action);
-  //     expect(mockSocket.on).toBeCalledWith([]);
+    expect(mockSocket.on).toHaveBeenCalledWith("chartData", expect.anything());
+  });
 
-  // })
+  it("it should dispatch chart data to the store", () => {
+    const action = { type: BOOTSTRAP };
+    const middleware = chartMiddleware({ socketService });
+    const updateAction = {
+      payload: [{ close: 1, date: "1" }],
+      type: "UPDATE_CHART_DATA"
+    };
+    middleware(store)(next)(action);
+
+    expect(store.dispatch).toHaveBeenCalledWith(updateAction);
+  });
 
   it('it should emit a stock of "AAPL" and a chart range of "1Y"', () => {
     const action = updateChartRangeAction("1Y");
